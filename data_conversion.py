f9f52f47e08507a0d14c358da46a9123afbf4e7d
etsa3591 <65090508+etsa3591@users.noreply.github.com>
2023-12-01 02:00:24 +0900

    Generate test cases

--- data_conversion.py
+++ data_conversion.py
@@ -6,6 +6,12 @@ from glob import glob
 import librosa as lb
 from librosa import display
 from tqdm import tqdm
+import random
+
+hop = 450
+either_side = 10
+fill_in = 2
+move_between = 2
 
 class DataConversion:
     def __init__(self, file_dir):
@@ -59,22 +65,44 @@ class DataConversion:
 
         print('Converting to Mel-Spectrogram:')
         for x in tqdm(range(len(self.data))):
-            S = lb.feature.melspectrogram(y=self.y[x], sr=self.sr[x], n_mels=128)
+            S = lb.feature.melspectrogram(y=self.y[x], sr=self.sr[x], n_mels=128, hop_length = hop)
             S_db_mel.append(lb.amplitude_to_db(S, ref=np.max))
-
+        for x in range(5):
+            print(self.y[x].shape)
+            print(self.sr[x])
+            print(S_db_mel[x].shape)
         return S_db_mel
+
+    def make_inputs_outputs(self, S_db_mel):
+        inputs = []
+        outputs = []
+        random.shuffle(S_db_mel)
+        print("Splitting files to generate test cases:")
+        for x in tqdm(range(len(S_db_mel))):
+            fps = int(self.sr[x] / hop)
+            mel = S_db_mel[x]
+            melt = np.transpose(mel)
+            for i in range(int(melt.shape[0]/(move_between * fps))):
+                first = melt[i * move_between * fps : (i * move_between + either_side) * fps]
+                middle = melt[(i * move_between + either_side) * fps : (i * move_between + either_side + fill_in) * fps]
+                end = melt[(i * move_between + either_side + fill_in) * fps : (i * move_between + either_side * 2 + fill_in) * fps]
+                inputs.append(np.concatenate((first, np.zeros(middle.shape), end)))
+                outputs.append(middle)
+        print(inputs[0].shape)
+        print(outputs[0].shape)
+        return (inputs, outputs)
     
     def display_mel(self, mel_list, num):
         fig, ax = plt.subplots(figsize=(10,5))
         img = display.specshow(mel_list[num], x_axis='time', y_axis='log', ax=ax)
         ax.set_title(f'File {self.data[num]} Mel-Spectrogram', fontsize=20)
         plt.show()
-
-
-# test1 = DataConversion('./dataset/*.mp3')
-# test1.load_data()
-# test1.display_raw_audio(20)
-# spect_list = test1.data_to_stft()
-# test1.display_stft(spect_list, 20)
-# spect_list_mel = test1.data_to_mel()
-# test1.display_mel(spect_list_mel, 20)
+        
+if __name__ == "__main__":
+    test1 = DataConversion('./dataset/*.mp3')
+    test1.load_data()
+    test1.display_raw_audio(20)
+    spect_list = test1.data_to_stft()
+    test1.display_stft(spect_list, 20)
+    spect_list_mel = test1.data_to_mel()
+    test1.display_mel(spect_list_mel, 20)
--- main.py
+++ main.py
@@ -3,10 +3,25 @@ from model import WaveNet
 import torch
 from torch.utils.data import random_split
 
+#print(WaveNet())
 test1 = DataConversion('./dataset/*.mp3')
 test1.load_data()
 spect_list_mel = test1.data_to_mel()
-test1.display_mel(spect_list_mel, 20)
+inputs, outputs = test1.make_inputs_outputs(spect_list_mel)
+print(inputs[0].shape)
+print(outputs[0].shape)
+#test1.display_mel(spect_list_mel, 20)
 
-trainSet, testSet, valSet = random_split(spect_list_mel, [.7, .2, .1])
+trainSet = [inputs[:int(len(inputs) * 0.7)],
+            outputs[:int(len(inputs) * 0.7)]]
+testSet = [inputs[int(len(inputs) * 0.7):int(len(inputs) * 0.9)],
+            outputs[int(len(inputs) * 0.7):int(len(inputs) * 0.9)]]
+valSet = [inputs[int(len(inputs) * 0.9):],
+            outputs[int(len(inputs) * 0.9):]]
+
 print(len(trainSet), len(testSet), len(valSet))
+print(trainSet[0][0].shape)
+
+# These are obviously incorrect values, just having something so it compiles
+model = WaveNet(1, 32, 32, 4)
+print(model)
