76462bc6980806baad3ba770520283b12724b157
Nicosia-Gino <ginonicosia@nevada.unr.edu>
2023-12-04 20:34:57 +0900

    Working model that trains and test | adjust hyperparameters

--- model.py
+++ model.py
@@ -63,3 +63,17 @@ class WaveNet(nn.Module):
 
         return x
 
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 1, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.conv3(x)
+        
+        return x
