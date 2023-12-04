57863267b790519906d75d486b7b983383807475
Nicosia-Gino <ginonicosia@nevada.unr.edu>
2023-12-05 01:47:00 +0900

    Fixed graph to detach from gradient and go to cpu before making numpy in case of using mps or cuda

--- data_conversion.py
+++ data_conversion.py
@@ -9,12 +9,7 @@ from tqdm import tqdm
 import random
 import copy
 
-hop = 128
-either_side = 10
-fill_in = 2
-move_between = 10
 global_sr = 22050
-n_mels = 128
 
 class DataConversion:
     def __init__(self, file_dir):
@@ -30,7 +25,6 @@ class DataConversion:
 
         for x in tqdm(range(len(self.data))):
             yt, srt = lb.load(self.data[x], sr = global_sr)
-            # TODO: Change this to upsample/downsample other sample rates
             assert srt == global_sr
             self.y.append(yt)
 
@@ -89,52 +83,12 @@ class DataConversion:
 
         return self.mel_cut, self.mel_full
 
-    # def data_to_mel(self, device):
-    #     S_db_mel = []
-
-    #     print('Converting to Mel-Spectrogram:')
-
-    #     for x in tqdm(range(len(self.data))):
-    #         if hop > 0: S = lb.feature.melspectrogram(y=self.y[x], sr=self.sr[x], n_mels=128, hop_length = hop)
-    #         else: S = self.y[x]
-    #         S_db_mel.append(lb.amplitude_to_db(S, ref=np.max))
-    #     for x in range(5):
-    #         print(self.y[x].shape)
-    #         print(self.sr[x])
-    #         print(S_db_mel[x].shape)
-    #     return S_db_mel
-
-    def make_inputs_outputs(self, S_db_mel):
-        inputs_before = []
-        inputs_after = []
-        outputs = []
-        random.shuffle(S_db_mel)
-        print("Splitting files to generate test cases:")
-        for x in tqdm(range(len(S_db_mel))):
-            fps = int(self.sr[x] / hop) if hop > 0 else self.sr[x]
-            mel = S_db_mel[x]
-            melt = np.transpose(mel)
-            for i in range(int(melt.shape[0]/(move_between * fps))):
-                first = melt[i * move_between * fps : (i * move_between + either_side) * fps]
-                middle = melt[(i * move_between + either_side) * fps : (i * move_between + either_side + fill_in) * fps]
-                end = melt[(i * move_between + either_side + fill_in) * fps : (i * move_between + either_side * 2 + fill_in) * fps]
-                inputs_before.append(first)
-                inputs_after.append(end)
-                outputs.append(middle)
-        for i in range(5):
-            print(inputs_before[i].shape)
-            print(inputs_after[i].shape)
-            print(outputs[i].shape)
-        return (inputs_before, inputs_after, outputs)
-    
     def display_mel(self, mel_list, num):
         if mel_list == 'full':
             mel_list = self.mel_full
         if mel_list == 'cut':
             mel_list = self.mel_cut
-        # fig, ax = plt.subplots(figsize=(10,5))
-        # img = display.specshow(mel_list[num], x_axis='time', y_axis='log', ax=ax)
-        # ax.set_title(f'File {self.data[num]} Mel-Spectrogram', fontsize=20)
+
         plt.figure(figsize=(10, 4))
         lb.display.specshow(lb.power_to_db(mel_list[num], ref=np.max), y_axis='mel', x_axis='time')
         plt.title(f'File {self.data[num]} Mel-Spectrogram')
